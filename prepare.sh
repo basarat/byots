1d3362970a925bcef47ca68f83afe87397c7a2f5
basarat <basaratali@gmail.com>
2016-07-24 21:09:10 +1000

    fix build

--- prepare.sh
+++ prepare.sh
@@ -13,18 +13,21 @@ npm install typescript@next --save-dev --save-exact
 cd ./TypeScript
 
 # Get latest
-# git clean -xfd # deletes any untracted existing artifacts including node_modules
+git clean -xfd # deletes any untracted existing artifacts including node_modules
 git fetch origin
 git reset --hard origin/master
 
 # Install everything
 npm install
 
+# Run a jake task to get the `diagnosticInformationMap.generated.ts` file
+npm run build:compiler
+
 # Copy our tsconfig.json to do the building
 cp ../src/tsconfig.json ./src/tsconfig.json
 
-# Now build the compiler to get new definition file
-./node_modules/.bin/tsc -p ./src
+# Now build the compiler with our tsconfig.json using nightly typescript to get new definitions
+../node_modules/.bin/tsc -p ./src
 
 # Finally just take the new typescript.d.ts
 mv ./built/local/typescriptServices.d.ts ../bin/typescript.d.ts
