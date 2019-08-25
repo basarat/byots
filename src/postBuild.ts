6aaddc644872a45411f0501fde7091668b2b8251
Basarat <basaratali@gmail.com>
2019-08-25 13:10:15 +1000

    [ci skip] Version: 3.7.0-dev.20190824.3.10

--- src/postBuild.ts
+++ src/postBuild.ts
@@ -8,16 +8,19 @@ import * as utils from './utils';
  * I think the `const enum` causes more pain than its worth for dev tools (everything needs to be rebuilt).
  * So change to enum to prevent inlining
  */
-const dtsFilePath = '../bin/typescript.d.ts';
-utils.writeFile(dtsFilePath, utils.readFile(dtsFilePath).replace(/const enum /g, 'enum '));
+const fixesForFiles: utils.FixesForFile[] = [
+    {
+        filePath: '../bin/typescript.d.ts',
+        fixes: [
+          {
+            orig: /const enum /g,
+            new: 'enum '
+          }
+        ]
+      },
+]
 
-/** 
- * Don't want preflogger 
- */
-utils.writeFile(dtsFilePath, utils.readFile(dtsFilePath).replace(`type PerfLogger = typeof import("@microsoft/typescript-etw");
-/** Performance logger that will generate ETW events if possible */
-export const perfLogger: PerfLogger;
-export {};`, ''));
+utils.runFixesforFiles(fixesForFiles);
 
 /**
  * Also get the version number for current ts nightly and use it to power our version number
