377e4a7d55dcee7bfa1d587a5c184b5e503f3f1a
basarat <basarat@users.noreply.github.com>
2022-07-07 01:35:39 +0000

    Apply automatic changes

--- bin/typescript.d.ts
+++ bin/typescript.d.ts
@@ -834,6 +834,10 @@ declare namespace ts {
         function formatSignatureFlags(flags: SignatureFlags | undefined): string;
         function formatObjectFlags(flags: ObjectFlags | undefined): string;
         function formatFlowFlags(flags: FlowFlags | undefined): string;
+        function formatRelationComparisonResult(result: RelationComparisonResult | undefined): string;
+        function formatCheckMode(mode: CheckMode | undefined): string;
+        function formatSignatureCheckMode(mode: SignatureCheckMode | undefined): string;
+        function formatTypeFacts(facts: TypeFacts | undefined): string;
         function printControlFlowGraph(flowNode: FlowNode): void;
         function formatControlFlowGraph(flowNode: FlowNode): string;
         function attachFlowNodeDebugInfo(flowNode: FlowNodeBase): void;
@@ -3298,6 +3302,12 @@ declare namespace ts {
          * It is _public_ so that (pre)transformers can set this field,
          * since it switches the builtin `node` module transform. Generally speaking, if unset,
          * the field is treated as though it is `ModuleKind.CommonJS`.
+         *
+         * Note that this field is only set by the module resolution process when
+         * `moduleResolution` is `Node16` or `NodeNext`, which is implied by the `module` setting
+         * of `Node16` or `NodeNext`, respectively, but may be overriden (eg, by a `moduleResolution`
+         * of `node`). If so, this field will be unset and source files will be considered to be
+         * CommonJS-output-format by the node module transformer and type checker, regardless of extension or context.
          */
         impliedNodeFormat?: ModuleKind.ESNext | ModuleKind.CommonJS;
         scriptKind: ScriptKind;
@@ -12742,6 +12752,101 @@ declare namespace ts {
     function createGetSymbolWalker(getRestTypeOfSignature: (sig: Signature) => Type, getTypePredicateOfSignature: (sig: Signature) => TypePredicate | undefined, getReturnTypeOfSignature: (sig: Signature) => Type, getBaseTypes: (type: Type) => Type[], resolveStructuredTypeMembers: (type: ObjectType) => ResolvedType, getTypeOfSymbol: (sym: Symbol) => Type, getResolvedSymbol: (node: Node) => Symbol, getConstraintOfTypeParameter: (typeParameter: TypeParameter) => Type | undefined, getFirstIdentifier: (node: EntityNameOrEntityNameExpression) => Identifier, getTypeArguments: (type: TypeReference) => readonly Type[]): (accept?: (symbol: Symbol) => boolean) => SymbolWalker;
 }
 declare namespace ts {
+    enum TypeFacts {
+        None = 0,
+        TypeofEQString = 1,
+        TypeofEQNumber = 2,
+        TypeofEQBigInt = 4,
+        TypeofEQBoolean = 8,
+        TypeofEQSymbol = 16,
+        TypeofEQObject = 32,
+        TypeofEQFunction = 64,
+        TypeofEQHostObject = 128,
+        TypeofNEString = 256,
+        TypeofNENumber = 512,
+        TypeofNEBigInt = 1024,
+        TypeofNEBoolean = 2048,
+        TypeofNESymbol = 4096,
+        TypeofNEObject = 8192,
+        TypeofNEFunction = 16384,
+        TypeofNEHostObject = 32768,
+        EQUndefined = 65536,
+        EQNull = 131072,
+        EQUndefinedOrNull = 262144,
+        NEUndefined = 524288,
+        NENull = 1048576,
+        NEUndefinedOrNull = 2097152,
+        Truthy = 4194304,
+        Falsy = 8388608,
+        IsUndefined = 16777216,
+        IsNull = 33554432,
+        IsUndefinedOrNull = 50331648,
+        All = 134217727,
+        BaseStringStrictFacts = 3735041,
+        BaseStringFacts = 12582401,
+        StringStrictFacts = 16317953,
+        StringFacts = 16776705,
+        EmptyStringStrictFacts = 12123649,
+        EmptyStringFacts = 12582401,
+        NonEmptyStringStrictFacts = 7929345,
+        NonEmptyStringFacts = 16776705,
+        BaseNumberStrictFacts = 3734786,
+        BaseNumberFacts = 12582146,
+        NumberStrictFacts = 16317698,
+        NumberFacts = 16776450,
+        ZeroNumberStrictFacts = 12123394,
+        ZeroNumberFacts = 12582146,
+        NonZeroNumberStrictFacts = 7929090,
+        NonZeroNumberFacts = 16776450,
+        BaseBigIntStrictFacts = 3734276,
+        BaseBigIntFacts = 12581636,
+        BigIntStrictFacts = 16317188,
+        BigIntFacts = 16775940,
+        ZeroBigIntStrictFacts = 12122884,
+        ZeroBigIntFacts = 12581636,
+        NonZeroBigIntStrictFacts = 7928580,
+        NonZeroBigIntFacts = 16775940,
+        BaseBooleanStrictFacts = 3733256,
+        BaseBooleanFacts = 12580616,
+        BooleanStrictFacts = 16316168,
+        BooleanFacts = 16774920,
+        FalseStrictFacts = 12121864,
+        FalseFacts = 12580616,
+        TrueStrictFacts = 7927560,
+        TrueFacts = 16774920,
+        SymbolStrictFacts = 7925520,
+        SymbolFacts = 16772880,
+        ObjectStrictFacts = 7888800,
+        ObjectFacts = 16736160,
+        FunctionStrictFacts = 7880640,
+        FunctionFacts = 16728000,
+        VoidFacts = 9830144,
+        UndefinedFacts = 26607360,
+        NullFacts = 42917664,
+        EmptyObjectStrictFacts = 83427327,
+        EmptyObjectFacts = 83886079,
+        UnknownFacts = 83886079,
+        AllTypeofNE = 556800,
+        OrFactsMask = 8256,
+        AndFactsMask = 134209471
+    }
+    enum CheckMode {
+        Normal = 0,
+        Contextual = 1,
+        Inferential = 2,
+        SkipContextSensitive = 4,
+        SkipGenericFunctions = 8,
+        IsForSignatureHelp = 16,
+        IsForStringLiteralArgumentCompletions = 32,
+        RestBindingElement = 64
+    }
+    enum SignatureCheckMode {
+        BivariantCallback = 1,
+        StrictCallback = 2,
+        IgnoreReturnTypes = 4,
+        StrictArity = 8,
+        Callback = 3
+    }
     function getNodeId(node: Node): number;
     function getSymbolId(symbol: Symbol): SymbolId;
     function isInstantiatedModule(node: ModuleDeclaration, preserveConstEnums: boolean): boolean;
